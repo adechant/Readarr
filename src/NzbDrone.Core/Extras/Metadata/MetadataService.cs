Lidarr/Lidarr

Fixed metadata images containing html content
(cherry picked from commit 87a64cdacbb0945c89b878d02a7eb2ac28427026)

src/NzbDrone.Core/Extras/Metadata/MetadataService.cs
@@ -492,6 +492,10 @@ namespace NzbDrone.Core.Extras.Metadata
                     _mediaFileAttributeService.SetFilePermissions(fullPath);
                 }
             }
+            catch (HttpException ex)
+            {
+                _logger.Warn(ex, "Couldn't download image {0} for {1}. {2}", image.Url, artist, ex.Message);
+            }
             catch (WebException ex)
             {
                 _logger.Warn(ex, "Couldn't download image {0} for {1}. {2}", image.Url, artist, ex.Message);

src/NzbDrone.Core/MediaCover/MediaCoverService.cs
@@ -220,6 +220,10 @@ namespace NzbDrone.Core.MediaCover
                         updated = true;
                     }
                 }
+                catch (HttpException e)
+                {
+                    _logger.Warn("Couldn't download media cover for {0}. {1}", album, e.Message);
+                }
                 catch (WebException e)
                 {
                     _logger.Warn("Couldn't download media cover for {0}. {1}", album, e.Message);

adechant/Readarr

src/NzbDrone.Core/Extras/Metadata/MetadataService.cs
@@ -368,6 +368,10 @@ namespace NzbDrone.Core.Extras.Metadata
                     _mediaFileAttributeService.SetFilePermissions(fullPath);
                 }
             }
+            catch (HttpException ex)
+            {
+                _logger.Warn(ex, "Couldn't download image {0} for {1}. {2}", image.Url, author, ex.Message);
+            }
             catch (WebException ex)
             {
                 _logger.Warn(ex, "Couldn't download image {0} for {1}. {2}", image.Url, author, ex.Message);

src/NzbDrone.Core/MediaCover/MediaCoverService.cs
@@ -143,6 +143,10 @@ namespace NzbDrone.Core.MediaCover
                         DownloadCover(author, cover, serverFileHeaders.LastModified ?? DateTime.Now);
                     }
                 }
+                catch (HttpException e)
+                {
+                    _logger.Warn("Couldn't download media cover for {0}. {1}", author, e.Message);
+                }
                 catch (WebException e)
                 {
                     _logger.Warn("Couldn't download media cover for {0}. {1}", author, e.Message);
@@ -192,6 +196,10 @@ namespace NzbDrone.Core.MediaCover
                     {
                         DownloadBookCover(book, cover, serverFileHeaders.LastModified ?? DateTime.Now);
                     }
+                }
+                catch (HttpException e)
+                {
+                    _logger.Warn("Couldn't download media cover for {0}. {1}", book, e.Message);
                 }
                 catch (WebException e)
                 {