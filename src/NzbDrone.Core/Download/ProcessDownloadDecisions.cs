Sonarr/Sonarr

Fixed: Pushed releases not being properly rejected

src/NzbDrone.Core/Download/ProcessDownloadDecisions.cs
@@ -131,6 +131,11 @@ namespace NzbDrone.Core.Download
                 return ProcessedDecisionResult.Skipped;
             }
 
+            if (!IsQualifiedReport(decision))
+            {
+                return ProcessedDecisionResult.Rejected;
+            }
+
             if (decision.TemporarilyRejected)
             {
                 _pendingReleaseService.Add(decision, PendingReleaseReason.Delay);
@@ -150,8 +155,13 @@ namespace NzbDrone.Core.Download
 
         internal List<DownloadDecision> GetQualifiedReports(IEnumerable<DownloadDecision> decisions)
         {
+            return decisions.Where(IsQualifiedReport).ToList();
+        }
+
+        internal bool IsQualifiedReport(DownloadDecision decision)
+        {
             // Process both approved and temporarily rejected
-            return decisions.Where(c => (c.Approved || c.TemporarilyRejected) && c.RemoteEpisode.Episodes.Any()).ToList();
+            return (decision.Approved || decision.TemporarilyRejected) && decision.RemoteEpisode.Episodes.Any();
         }
 
         private bool IsEpisodeProcessed(List<DownloadDecision> decisions, DownloadDecision report)

adechant/Readarr

(cherry picked from commit 07f816ffb18ac34090c2f8ba25147737299b361d)
Closes #2943

src/NzbDrone.Core/Download/ProcessDownloadDecisions.cs
@@ -131,6 +131,11 @@ namespace NzbDrone.Core.Download
                 return ProcessedDecisionResult.Skipped;
             }
 
+            if (!IsQualifiedReport(decision))
+            {
+                return ProcessedDecisionResult.Rejected;
+            }
+
             if (decision.TemporarilyRejected)
             {
                 _pendingReleaseService.Add(decision, PendingReleaseReason.Delay);
@@ -150,8 +155,13 @@ namespace NzbDrone.Core.Download
 
         internal List<DownloadDecision> GetQualifiedReports(IEnumerable<DownloadDecision> decisions)
         {
-            //Process both approved and temporarily rejected
-            return decisions.Where(c => (c.Approved || c.TemporarilyRejected) && c.RemoteBook.Books.Any()).ToList();
+            return decisions.Where(IsQualifiedReport).ToList();
+        }
+
+        internal bool IsQualifiedReport(DownloadDecision decision)
+        {
+            // Process both approved and temporarily rejected
+            return (decision.Approved || decision.TemporarilyRejected) && decision.RemoteBook.Books.Any();
         }
 
         private bool IsBookProcessed(List<DownloadDecision> decisions, DownloadDecision report)